57b242b7de4a14f0fce6ea833c5576c706262d56
Ctrl-Eyob <eyobaberabekru@gmai.com>
2025-11-13 12:55:29 +0300

    edited

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,5 +1,79 @@
 // task_2/js/main.ts
 
+// Director Interface
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+// Teacher Interface
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+// Director class
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+// Teacher class
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+// Create employee function
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+// Test
+// console.log(createEmployee(200));
+// console.log(createEmployee(1000));
+// console.log(createEmployee('$500'));
+
+// Type predicate
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+// Execute work
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) return employee.workDirectorTasks();
+  else return employee.workTeacherTasks();
+}
+
+// Test
+// console.log(executeWork(createEmployee(200)));  // Getting to work
+// console.log(executeWork(createEmployee(1000))); // Getting to director tasks
+
+// String literal type
 interface Employee {
   firstName: string;
   lastName: string;
@@ -34,4 +108,8 @@ const emp1: Employee = {
 printEmployeeDetails(emp1);
 
 console.log(calculateBonus(400)); // Salary is too low for a bonus
-console.log(calculateBonus(800)); // You are eligible for a bonus
+console.log(calculateBonus(800)); // You are eligible for a bonus}
+
+// Test
+// console.log(teachClass('Math'));
+// console.log(teachClass('History'));
