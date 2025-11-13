34e2fa966e25fdeb3ca740ec3dbc62c655e4f06f
Ctrl-Eyob <eyobaberabekru@gmai.com>
2025-11-13 12:41:56 +0300

    corrected the string name

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,19 +1,25 @@
-// task_1/js/main.ts
 
-// Interface describing the constructor
 interface StudentConstructor {
   new (firstName: string, lastName: string): StudentClassInterface;
 }
 
-// Interface describing the class
+// Interface describing the structure of the class
 interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
   workOnHomework(): string;
   displayName(): string;
 }
 
-// Implementing the class
+// The actual class implementation
 class StudentClass implements StudentClassInterface {
-  constructor(public firstName: string, public lastName: string) {}
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 
   workOnHomework(): string {
     return 'Currently working';
@@ -24,7 +30,7 @@ class StudentClass implements StudentClassInterface {
   }
 }
 
-// Example (optional):
+// Example test (not required for submission)
 // const student = new StudentClass('Eyob', 'Getachew');
-// console.log(student.displayName()); // Eyob
-// console.log(student.workOnHomework()); // Currently working
+// console.log(student.displayName());  // Eyob
+// console.log(student.workOnHomework());  // Currently working
